ce95ade5af4fb61b8920a6a9605791505e5b0eee
thanakarn <thanakarn@artisan.co.th>
2020-08-05 22:59:38 +0700

    first commit with next.js

---
+++ pages/components/Header/header.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Header() {
+  return (
+    <div>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/antd/4.5.2/antd.min.css"
+          integrity="sha512-3w6Dnr9oqweHnMzLxXRdLWpmgf3PSMqob+BZWqnyhXtrs7ibtqdxZEAT5PTKw4D+eXm+5FUm8SpS/PlxLVncWw=="
+        />
+      </Head>
+    </div>
+  );
+}
